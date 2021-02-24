f5670bafae92a994752ef15bbd2d2e2fe817909d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-24 19:24:27 +0100

    G: Use goodix HAL from daisy
    * From daisy-user 10 QKQ1.191002.002 V11.0.18.0.QDLMIXM release-keys
    
    Change-Id: I037b0b833c0fa06c7e9998dcf1b2b1dcff9fb274

--- extract-files.sh
+++ extract-files.sh
@@ -61,23 +61,14 @@ function blob_fixup() {
         vendor/lib/libmpbase.so)
             "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
             ;;
-        vendor/lib64/hw/gf_fingerprint.goodix.default.so \
-        |vendor/lib64/libgoodixfingerprintd_binder.so \
-        |vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so \
-        |vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so)
-            "${PATCHELF_0_8}" --remove-needed "libbacktrace.so" "${2}"
-            "${PATCHELF_0_8}" --remove-needed "libkeystore_binder.so" "${2}"
-            "${PATCHELF_0_8}" --remove-needed "libkeymaster_messages.so" "${2}"
-            "${PATCHELF_0_8}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
-            "${PATCHELF_0_8}" --remove-needed "libsoftkeymaster.so" "${2}"
-            "${PATCHELF_0_8}" --remove-needed "libsoftkeymasterdevice.so" "${2}"
-            "${PATCHELF_0_8}" --remove-needed "libunwind.so" "${2}"
-            ;;
         vendor/lib64/libril-qc-hal-qmi.so)
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
             ;;
         vendor/lib64/libsettings.so)
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
+            ;;
+        vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so)
+            "${PATCHELF_0_8}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
             ;;
     esac
 }
