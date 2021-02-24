89e00dc16ffc2aef7ca3b8fec9fb94eabedda20b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-24 01:49:27 +0100

    G: Remove libandroid.so dependency from libmpbase.so
    Change-Id: I334b51c334ba8bd64e526e62a459d84e671c8230

--- extract-files.sh
+++ extract-files.sh
@@ -58,6 +58,9 @@ function blob_fixup() {
         product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml|product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
             sed -i 's/version="2.0"/version="1.0"/g' "${2}"
             ;;
+        vendor/lib/libmpbase.so)
+            "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
+            ;;
         vendor/lib64/libril-qc-hal-qmi.so)
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
             ;;
