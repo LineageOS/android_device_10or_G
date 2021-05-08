ad6293aae0a5bb024a780472e0e3684f1443d5da
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-05-08 22:30:15 +0200

    G: Build custom fingerprint HIDL
    Change-Id: Ifd730afca08f87e87709c1f7d37166d9e52e1c18

--- device.mk
+++ device.mk
@@ -152,6 +152,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4-service.clearkey
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.G
+
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
