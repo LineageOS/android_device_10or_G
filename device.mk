12f4d946fd76e1792079a2897d4f8112125638f4
Debayan Kar <kardebayan3@gmail.com>
2021-12-13 16:02:56 +0530

    G: Build vendor variant of android.hardware.gnss@2.0
    Change-Id: Id6e45a217e811edbb76ac445e709a3cd0d799f28

--- device.mk
+++ device.mk
@@ -160,6 +160,10 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+# GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@2.0.vendor
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
