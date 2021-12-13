82b344caa61fce27e79cdbb9b25e6ca46a014642
Debayan Kar <kardebayan3@gmail.com>
2021-12-13 16:11:22 +0530

    G: Build vendor variant of vendor.qti.hardware.camera.device@1.0
    * Fixes :-
    
    - E vndksupport: Could not load /vendor/lib/hw/android.hardware.camera.provider@2.4-impl.so from default namespace: dlopen failed: library "vendor.qti.hardware.camera.device@1.0.so" not found: needed by /vendor/lib/android.hardware.camera.provider@2.4-external.so in namespace (default).
    
    Change-Id: I8acd2203379531d842846541da76427c1e95b843

--- device.mk
+++ device.mk
@@ -183,7 +183,8 @@ PRODUCT_PACKAGES += \
 
 # IMS
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.camera.device@1.0
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # HW crypto
 PRODUCT_PACKAGES += \
