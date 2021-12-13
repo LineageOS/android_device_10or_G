2674ae7fc9ec4f92d4d6fa8c830efcd5bb9199ca
Debayan Kar <kardebayan3@gmail.com>
2021-12-13 18:52:28 +0530

    G: Build more radio interfaces
    Change-Id: I72c6ec8ccab0b9f350626618feb2b73e10215c6d

--- device.mk
+++ device.mk
@@ -274,7 +274,9 @@ PRODUCT_PACKAGES += \
 
 # Radio
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.4.vendor \
     android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.0.vendor
 
 # Ramdisk
