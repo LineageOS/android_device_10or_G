81bcb01fceaa5ed52f70f36e0fdeeb6045858695
Debayan Kar <kardebayan3@gmail.com>
2021-12-13 17:45:46 +0530

    G: Build vendor variant of android.hardware.power@1.2
    * Fixes :-
    
    - E vndksupport: Could not load /vendor/lib64/hw/android.hardware.gnss@2.0-impl-qti.so from default namespace: dlopen failed: library "android.hardware.power@1.2.so" not found: needed by /vendor/lib64/hw/android.hardware.gnss@2.0-impl-qti.so in namespace (default).
    
    Change-Id: Ic34187f4784d8065add844ecbf03f29690645698

--- device.mk
+++ device.mk
@@ -258,7 +258,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power@1.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
