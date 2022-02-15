f3266998be36cd872d87615a6a3e0ef4b4b3bfef
ZeeLog <Danjel.Zammit@gmail.com>
2022-02-15 10:30:23 +0100

    G: Cleanup telephony packages and blobs
    * no longer needed

--- device.mk
+++ device.mk
@@ -333,11 +333,7 @@ PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
-    qti_telephony_utils.xml \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+    qti_telephony_utils.xml
 
 # Thermal
 PRODUCT_COPY_FILES += \
