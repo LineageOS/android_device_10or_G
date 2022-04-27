cd61188b3221586ef404597854df5c8f83a45c93
=?UTF-8?q?Daniel=20K=C3=B6ster?= <dk@eada.de>
2022-04-27 16:52:30 +0200

    G: Remove izat
    Change-Id: I6d8bcc60c4c731790f416fe3e992dd7035552e47

--- device.mk
+++ device.mk
@@ -169,7 +169,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
     $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
-    $(LOCAL_PATH)/configs/gps/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
     $(LOCAL_PATH)/configs/gps/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
     $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf
 
