0506acbbc2f38b22e64760c7177c5fab4d2982eb
me-cafebabe <me_cafebabe@hotmail.com>
2022-07-22 01:42:26 +0530

    G: Setup Retrofit Dynamic Partitions

--- device.mk
+++ device.mk
@@ -149,6 +149,11 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4-service.clearkey
 
+# Dynamic Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.G
@@ -291,6 +296,7 @@ PRODUCT_PACKAGES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
+    fstab.qcom_ramdisk \
     fstab.qcom \
     init.msm.usb.configfs.rc \
     init.qcom.rc \
