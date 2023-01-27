44cadaa0a865d0629b083da20fc8e14c39ee2bd9
Debayan Kar <69067988+kardebayan@users.noreply.github.com>
2023-01-27 18:19:15 +0000

    G: Drop FDE leftovers

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,9 +82,6 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
-# HW crypto
-TARGET_HW_DISK_ENCRYPTION := true
-
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_G
 TARGET_RECOVERY_DEVICE_MODULES := libinit_G
--- device.mk
+++ device.mk
@@ -195,12 +195,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0.vendor
 
-# HW crypto
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
-TARGET_EXCLUDE_CRYPTFSHW := true
-
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/ft5435_ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ft5435_ts.kl \
