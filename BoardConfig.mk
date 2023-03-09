6efafa4a2446219e2ef7ba64ea61d30ea419c697
Debayan Kar <kardebayan3@gmail.com>
2023-03-09 15:12:58 +0530

    G: Switch to lineageos_holland2 defconfig
    Change-Id: I7ee8bba39e14b824f6d0d753194edd2461727e38

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@ BOARD_KERNEL_CMDLINE += androidboot.android_dt_dir=/non-existent androidboot.boo
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_KERNEL_CONFIG := holland2_defconfig
+TARGET_KERNEL_CONFIG := lineageos_holland2_defconfig
 TARGET_KERNEL_SOURCE := kernel/10or/G
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_VERSION := 4.9
